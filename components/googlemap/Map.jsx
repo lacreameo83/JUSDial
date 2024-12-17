7d7d98b5e166a03955a3abecc110ebb47d7426ba
precious osonwa <142677806+lacreameo83@users.noreply.github.com>
2024-12-17 14:24:04 +0100

    resolving conflits

--- components/googlemap/Map.jsx
+++ components/googlemap/Map.jsx
@@ -1,38 +1,11 @@
-// "use client";
-// import React from "react";
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-// import L from "leaflet";
+import React from 'react'
 
-// // Fix default marker icon issue
-// delete L.Icon.Default.prototype._getIconUrl;
+function Map() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-// L.Icon.Default.mergeOptions({
-//   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-//   iconRetinaUrl:
-//     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-//   shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
-// });
-
-// const WorldMap = () => {
-//   return (
-//     <MapContainer
-//       center={[51.505, -0.09]} // Center of the map
-//       zoom={13} // Initial zoom level
-//       scrollWheelZoom={false} // Disable zooming with mouse wheel
-//       style={{ height: "400px", width: "100%" }} // Map dimensions
-//     >
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       <Marker position={[51.505, -0.09]}>
-//         <Popup>
-//           A pretty CSS3 popup. <br /> Easily customizable.
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-//   );
-// };
-
-// export default WorldMap;
+export default Map
